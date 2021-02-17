25c5d43cb07d7ad3ad09533acb61709af5fb7a88
Reinaldo <reinaldoweb@hotmail.com>
2021-02-17 08:07:04 -0300

    Arquivo de configuraçao de apload

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,8 +1,8 @@
-import multer from 'multer';
+import multer from 'multer';//biblioteca para importação de imagem
 import path from 'path';
 export default{
 storage: multer.diskStorage({
-  destination: path.resolve(__dirname, '..', '..','uploads'),
+  destination: path.resolve(__dirname, '..', '..','uploads'),//SAber a rai do projeto
   filename:(req, file, cb)=>{
     const ext = path.extname(file.originalname);//Armazena a extenção da imagem na variavael ext
     const name = path.basename(file.originalname, ext); // Pega o nome original da imagem
