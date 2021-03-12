3e5ab70cec52fc545ef490592fa8828c98e29f08
Reinaldo <reinaldoweb@hotmail.com>
2021-03-12 10:40:47 -0300

    Criando reservas

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 const routes = new Router();;
 const upload = multer(uploadConfig)
@@ -17,5 +18,8 @@ routes.get('/houses', HouseController.index);//Lista as casas cadastradas
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
+
 
 export default routes;
